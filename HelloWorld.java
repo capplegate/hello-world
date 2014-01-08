c016b9fdb3aa27e6b7080ab30505f23aa2e4291d
capplegate <chris.applegate@live.co.uk>
2014-01-08 16:32:33 +0000

    entered constant path

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,23 +5,23 @@ public final class HelloWorld
 	public static void main(String[] args)
 	{
 		try
-		{String id = args[0];
-		String dir = args[1];
-		String outputDir = dir + id + "/";
+		{
+		String id = args[0];
+		String dir = "/data/output/appresults/" + id + "/folder/";
 		String outputFilename = "file.txt"; 
 		
 		// if the directory does not exist, create it
-		File theDir = new File(outputDir);
+		File theDir = new File(dir);
 		if (!theDir.exists())
 		{
 			theDir.mkdirs();  
 		}
 
 		System.out.println("done");
-		PrintWriter writer = new PrintWriter(outputDir + outputFilename, "UTF-8");
+		PrintWriter writer = new PrintWriter(dir + outputFilename, "UTF-8");
 		writer.println("Hello World!!!");
 		writer.close();
-		System.out.println("Data output to " + outputFilename);
+		System.out.println("Data output to " + dir + outputFilename);
 		System.out.println("Process completed!");
 		}
 		catch(Exception e)
